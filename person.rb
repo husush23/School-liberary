ec4c08f1ec1599239ab890a1cad24276d7e70a4a
husush23 <husseinkadare40@gmail.com>
2023-08-01 14:26:06 +0300

    Added changes

--- person.rb
+++ person.rb
@@ -32,7 +32,7 @@ end
 
 
 person = Person.new(name: 'Maximilianus', age: 22)
-puts person.correct_name 
+puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
