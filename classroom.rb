f649c58521abb72a954e1eaa7ac361cf87f0cba0
husush23 <husseinkadare40@gmail.com>
2023-08-02 12:17:52 +0300

    Classroom class created

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+require_relative "student"
+class Claasroom
+    attr_accessor :label, :students
+
+    def  initialize(label)
+        @label = label
+        @students = []
+    end
+    
+    def add_student(student)
+        @students << student
+        student.classroom = self
+end
