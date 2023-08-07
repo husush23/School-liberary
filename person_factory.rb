14b28e227a5e59ea8a5282db1fed907e7e699730
techmoves <muhndlovu28@gmail.com>
2023-08-07 18:23:03 +0200

    Fix bug

--- person_factory.rb
+++ person_factory.rb
@@ -5,8 +5,7 @@ class PersonFactory
     def create_student(age, name, parent_permission, classroom)
       Student.new(age: age, name: name, parent_permission: parent_permission, classroom: classroom)
     end
-    
-  
+     
     def create_teacher(age, name, specialization)
       Teacher.new(age:age, name:name, specialization:specialization)
     end
